fe9b1babb82c5083845d17d26f8785eea431bb31
Alessandro Bortolin <43854807+bortoz@users.noreply.github.com>
2022-02-03 04:41:32 +0000

    task-maker-rust 0.5.2
    Closes #5.
    
    Signed-off-by: Alessandro Bortolin <43854807+bortoz@users.noreply.github.com>

--- Formula/task-maker-rust.rb
+++ Formula/task-maker-rust.rb
@@ -1,8 +1,8 @@
 class TaskMakerRust < Formula
   desc "Improved version of cmsMake, rewritten in Rust"
   homepage "https://edomora97.github.io/task-maker-rust/"
-  url "https://github.com/edomora97/task-maker-rust/archive/v0.5.1.tar.gz"
-  sha256 "10ef9635d70e70fb105f1c4ec7bada0e4caa3aabd61287b1bf7cc53e3b908223"
+  url "https://github.com/edomora97/task-maker-rust/archive/v0.5.2.tar.gz"
+  sha256 "f770a980ec5bb32305bebcc75a2b90a2e07b81198ac0450d9dc86c4d9f54b4fa"
   license "MPL-2.0"
   head "https://github.com/edomora97/task-maker-rust.git"
 
