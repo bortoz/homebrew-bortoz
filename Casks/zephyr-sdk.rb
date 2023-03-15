da06973246bb71ff98dabcf3ef49a318ca3c28df
runner <runner@Mac-1678875690229.local>
2023-03-15 10:25:01 +0000

    Update zephyr-sdk from 0.15.2 to 0.16.0

--- Casks/zephyr-sdk.rb
+++ Casks/zephyr-sdk.rb
@@ -2,9 +2,9 @@ cask "zephyr-sdk" do
   arch arm:   "aarch64",
        intel: "x86_64"
 
-  version "0.15.2"
-  sha256 arm:   "847ba3d83767389fbbf262f14f06baf857f4baf04c739edebd72601c4cc2bc80",
-         intel: "326d79577ff7660fdf05743a07dab6cfc01fafe5e571aced89a35c392a386238"
+  version "0.16.0"
+  sha256 arm:   "3ed78b2fe0fe539ac7761ed5ce0b2a23b87a0b76060cb5d201dbb989dbd83dfc",
+         intel: "d73b677c0fd16f7d6737c494d68c11d1446186a61d4a0154da910561f0de6802"
 
   url "https://github.com/zephyrproject-rtos/sdk-ng/releases/download/v#{version}/zephyr-sdk-#{version}_macos-#{arch}.tar.xz",
       verified: "github.com/zephyrproject-rtos/sdk-ng/"
