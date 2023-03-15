c6bc43ee2e99b37abd45beb7b21fd803a3d74492
Alessandro Bortolin <43854807+bortoz@users.noreply.github.com>
2023-03-15 11:20:33 +0100

    zephyr-sdk: fix download url

--- Casks/zephyr-sdk.rb
+++ Casks/zephyr-sdk.rb
@@ -6,7 +6,7 @@ cask "zephyr-sdk" do
   sha256 arm:   "847ba3d83767389fbbf262f14f06baf857f4baf04c739edebd72601c4cc2bc80",
          intel: "326d79577ff7660fdf05743a07dab6cfc01fafe5e571aced89a35c392a386238"
 
-  url "https://github.com/zephyrproject-rtos/sdk-ng/releases/download/v#{version}/zephyr-sdk-#{version}_macos-#{arch}.tar.gz",
+  url "https://github.com/zephyrproject-rtos/sdk-ng/releases/download/v#{version}/zephyr-sdk-#{version}_macos-#{arch}.tar.xz",
       verified: "github.com/zephyrproject-rtos/sdk-ng/"
   name "Zephyr SDK"
   desc "Zephyr Software Development Kit"
