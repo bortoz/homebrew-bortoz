81e301c5b76bfd1870820ac2a7440e096cade447
Alessandro Bortolin <43854807+bortoz@users.noreply.github.com>
2025-09-13 10:05:06 +0000

    task-maker-rust: update 0.6.16 bottle.

--- Formula/task-maker-rust.rb
+++ Formula/task-maker-rust.rb
@@ -11,10 +11,10 @@ class TaskMakerRust < Formula
   end
 
   bottle do
-    root_url "https://github.com/bortoz/homebrew-bortoz/releases/download/task-maker-rust-0.6.14"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "ecc45bde043c5200cab8d150f4cda5f45af6d34b35152b113b2cbf3ab37dc46f"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "837f54c69d52d2e5e745e25bacbc62fa31a84d7e21f04d356289d7471eeaa950"
-    sha256 cellar: :any_skip_relocation, ventura:       "3384e6e0e2008b0cc37fa96c413b13dd913d78d7cd7e4187ec279a6df5e5ac4f"
+    root_url "https://github.com/bortoz/homebrew-bortoz/releases/download/task-maker-rust-0.6.16"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "9a04f4a44d1bb24a76c768a5a741ee6d9f704c64d7a46755b8f98dc02c7fbaad"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "37cb37430eabc62ba0a3474fa92f5a16bcce8991c8bb6bce204d5de2a4926193"
+    sha256 cellar: :any_skip_relocation, ventura:       "745f3093bf5668a83a97d12f0ec3d834ceeb47925a30cce7fe85ded13f7de7da"
   end
 
   depends_on "rust" => :build
